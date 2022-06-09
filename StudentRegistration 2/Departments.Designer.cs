074761b58b9a044e1035b75f0bd9d0f7f91e9e82
ilyasBayram <ilyas.bayram@hotmail.com>
2022-06-09 17:32:44 +0300

    deletion from depertment form is added. data in choosen cell is coming to txboxes.

--- StudentRegistration 2/Departments.Designer.cs
+++ StudentRegistration 2/Departments.Designer.cs
@@ -31,57 +31,23 @@ namespace StudentRegistration_2
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Departments));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.lblDepartmenID = new System.Windows.Forms.Label();
             this.txBoxDepartmentID = new System.Windows.Forms.TextBox();
             this.txBoxDepartmentName = new System.Windows.Forms.TextBox();
             this.lblDepartmentNmae = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dbDormitoryRegistrationDataSet = new StudentRegistration_2.DbDormitoryRegistrationDataSet();
-            this.tblDepartmentBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.tblDepartmentTableAdapter = new StudentRegistration_2.DbDormitoryRegistrationDataSetTableAdapters.TblDepartmentTableAdapter();
             this.departmentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.departmentNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.tblDepartmentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dbDormitoryRegistrationDataSet = new StudentRegistration_2.DbDormitoryRegistrationDataSet();
+            this.tblDepartmentTableAdapter = new StudentRegistration_2.DbDormitoryRegistrationDataSetTableAdapters.TblDepartmentTableAdapter();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dbDormitoryRegistrationDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblDepartmentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDormitoryRegistrationDataSet)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(494, 29);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(74, 57);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(598, 29);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(74, 57);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(698, 29);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(74, 57);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // lblDepartmenID
             // 
@@ -95,6 +61,7 @@ namespace StudentRegistration_2
             // 
             // txBoxDepartmentID
             // 
+            this.txBoxDepartmentID.Enabled = false;
             this.txBoxDepartmentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txBoxDepartmentID.Location = new System.Drawing.Point(249, 22);
             this.txBoxDepartmentID.Name = "txBoxDepartmentID";
@@ -133,20 +100,7 @@ namespace StudentRegistration_2
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(799, 286);
             this.dataGridView1.TabIndex = 7;
-            // 
-            // dbDormitoryRegistrationDataSet
-            // 
-            this.dbDormitoryRegistrationDataSet.DataSetName = "DbDormitoryRegistrationDataSet";
-            this.dbDormitoryRegistrationDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // tblDepartmentBindingSource
-            // 
-            this.tblDepartmentBindingSource.DataMember = "TblDepartment";
-            this.tblDepartmentBindingSource.DataSource = this.dbDormitoryRegistrationDataSet;
-            // 
-            // tblDepartmentTableAdapter
-            // 
-            this.tblDepartmentTableAdapter.ClearBeforeFill = true;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // departmentIDDataGridViewTextBoxColumn
             // 
@@ -165,41 +119,81 @@ namespace StudentRegistration_2
             this.departmentNameDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.departmentNameDataGridViewTextBoxColumn.Name = "departmentNameDataGridViewTextBoxColumn";
             // 
+            // tblDepartmentBindingSource
+            // 
+            this.tblDepartmentBindingSource.DataMember = "TblDepartment";
+            this.tblDepartmentBindingSource.DataSource = this.dbDormitoryRegistrationDataSet;
+            // 
+            // dbDormitoryRegistrationDataSet
+            // 
+            this.dbDormitoryRegistrationDataSet.DataSetName = "DbDormitoryRegistrationDataSet";
+            this.dbDormitoryRegistrationDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblDepartmentTableAdapter
+            // 
+            this.tblDepartmentTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDelete.BackgroundImage")));
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDelete.Location = new System.Drawing.Point(689, 29);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 62);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEdit.BackgroundImage")));
+            this.btnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnEdit.Location = new System.Drawing.Point(594, 29);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 62);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAdd.BackgroundImage")));
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAdd.Location = new System.Drawing.Point(494, 29);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 62);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
             // Departments
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.txBoxDepartmentName);
             this.Controls.Add(this.lblDepartmentNmae);
             this.Controls.Add(this.txBoxDepartmentID);
             this.Controls.Add(this.lblDepartmenID);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Departments";
             this.Text = "Departments";
             this.Load += new System.EventHandler(this.Departments_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dbDormitoryRegistrationDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblDepartmentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDormitoryRegistrationDataSet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label lblDepartmenID;
         private System.Windows.Forms.TextBox txBoxDepartmentID;
         private System.Windows.Forms.TextBox txBoxDepartmentName;
@@ -210,5 +204,8 @@ namespace StudentRegistration_2
         private DbDormitoryRegistrationDataSetTableAdapters.TblDepartmentTableAdapter tblDepartmentTableAdapter;
         private System.Windows.Forms.DataGridViewTextBoxColumn departmentIDDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn departmentNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
--- StudentRegistration 2/Departments.cs
+++ StudentRegistration 2/Departments.cs
@@ -18,7 +18,16 @@ namespace StudentRegistration_2
             InitializeComponent();
         }
 
+
         SqlConnection connection = new SqlConnection(@"Data Source=NB3401-0011;Initial Catalog=DbDormitoryRegistration;Integrated Security=True");
+
+        private void emptyTxBox()
+        {
+            if (txBoxDepartmentName==null)
+            {
+
+            }
+        }
 
         private void Departments_Load(object sender, EventArgs e)
         {
@@ -27,22 +36,67 @@ namespace StudentRegistration_2
 
         }
 
-        private void pictureBox3_Click(object sender, EventArgs e)
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txBoxDepartmentName.Text=="")
+            {
+                MessageBox.Show("Department name con not be empty", "Warning");
+            }
+            else
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand commandAdd = new SqlCommand("insert into TblDepartment (DepartmentName) values (@p1)", connection);
+                    commandAdd.Parameters.AddWithValue("@p1", txBoxDepartmentName.Text);
+                    commandAdd.ExecuteNonQuery();
+                    MessageBox.Show("New departmen is added");
+                    this.tblDepartmentTableAdapter.Fill(this.dbDormitoryRegistrationDataSet.TblDepartment);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Warning!! Problem occured. Please try again");
+                }
+            }
+            
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
         {
             try
             {
                 connection.Open();
-                SqlCommand command = new SqlCommand("insert into TblDepartment (DepartmentName) values (@p1)", connection);
-                command.Parameters.AddWithValue("@p1", txBoxDepartmentName.Text);
-                command.ExecuteNonQuery();
-                MessageBox.Show("New departmen is added");
+                SqlCommand commandDelete = new SqlCommand("delete from TblDepartment where departmentID =@p1", connection);
+                commandDelete.Parameters.AddWithValue("@p1", txBoxDepartmentID.Text);
+                commandDelete.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Deletion is successfull");
+                this.tblDepartmentTableAdapter.Fill(this.dbDormitoryRegistrationDataSet.TblDepartment);
             }
             catch (Exception)
             {
-
                 MessageBox.Show("Warning!! Problem occured. Please try again");
+
             }
             
+
+        }
+        int choosen;
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string departmentID, departmentName;
+            choosen = dataGridView1.SelectedCells[0].RowIndex;
+            departmentID = dataGridView1.Rows[choosen].Cells[0].Value.ToString();
+            departmentName = dataGridView1.Rows[choosen].Cells[1].Value.ToString();
+            txBoxDepartmentID.Text = departmentID;
+            txBoxDepartmentName.Text = departmentName;
+
         }
     }
 }
